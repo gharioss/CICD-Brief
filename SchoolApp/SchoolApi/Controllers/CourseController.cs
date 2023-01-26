52f523caeab0dbbd3b0f27f4f16b34d19b2c8284
gharioss <njulie818@gmail.com>
2023-01-26 11:53:43 +0100

    try pull_request ci dotnet

--- SchoolApp/SchoolApi/Controllers/CourseController.cs
+++ SchoolApp/SchoolApi/Controllers/CourseController.cs
@@ -5,7 +5,7 @@ using SchoolApi.Models;
 
 namespace SchoolApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class CourseController : ControllerBase
     {
